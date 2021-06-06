bc1eee36d9deb004842e64daa8eb7e2f3194609b
ifurther <55025025+ifurther@users.noreply.github.com>
2021-06-07 04:28:55 +0800

    add some command

--- gcpcloudshell.sh
+++ gcpcloudshell.sh
@@ -7,3 +7,14 @@ CheckProjectID(){
 		gcloud config set project $project_id
 	fi
 }
+
+GETACCESSTOKEN(){
+	ACCESS_TOKEN=$(gcloud auth print-access-token)
+	echo $ACCESS_TOKEN
+}
+
+GETAPIKEY(){
+	API_KEY=$(curl -X POST https://apikeys.googleapis.com/v1/projects/$PROJECT_ID/apiKeys?access_token=$ACCESS_TOKEN | jq -r ".currentKey")
+
+	echo $API_KEY
+}

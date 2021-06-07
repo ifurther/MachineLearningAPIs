67c1c75656aaf8256ad0734caea2706ccefcd222
ifurther <55025025+ifurther@users.noreply.github.com>
2021-06-07 18:23:00 +0800

    add bash check

--- createbucket.sh
+++ createbucket.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -e $1 ] && [ -e $DEVSHELL_PROJECT_ID ];then
+if [ ! -e $1 ] && [[ $DEVSHELL_PROJECT_ID ]];then
 Bucket_id=$DEVSHELL_PROJECT_ID;
-elif [ -e $1 ];then
+elif [[ $1 ]];then
 Bucket_id=$1
 else
 echo 'please check project id'
 fi
 export Bucket_id=$Bucket_id
-gsutil mb -c multi_regional gs://${Bucket_id}/ #2> success-stderr > success-stdout
+gsutil mb -c multi_regional gs://${Bucket_id} #2> success-stderr > success-stdout

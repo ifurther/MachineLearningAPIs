9e1116e0e9e37c053f0679fcfe23113c5d628b5d
ifurther <55025025+ifurther@users.noreply.github.com>
2021-06-07 18:38:10 +0800

    fix error

--- Uploadbucket.sh
+++ Uploadbucket.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -e $1 ];then
+if [[ $1 ]];then
 FILE_id=$1
 else
 echo 'please check file name'
 fi
 
-if [ -e $Bucket_id ];then
+if [[ $Bucket_id ]];then
 gsutil cp $FILE_id gs://$Bucket_id/FILE_id
 else
 echo 'please check project id'

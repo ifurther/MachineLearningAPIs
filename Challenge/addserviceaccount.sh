0d521dfcf207722341e90b292bd1b6bef7f94492
ifurther <55025025+ifurther@users.noreply.github.com>
2021-06-09 01:34:26 +0800

    fix role

--- Challenge/addserviceaccount.sh
+++ Challenge/addserviceaccount.sh
@@ -1,6 +1,6 @@
 gcloud iam service-accounts create my-account --display-name my-account
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:my-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role=roles/bigquery.admin
-gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:my-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role=roles/storage.objectAdmin
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:my-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role=roles/storage.admin
 gcloud iam service-accounts keys create key.json --iam-account=my-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
 export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/key.json
 

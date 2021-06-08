7744a48317f92e437a04c6a02097ff6e70253ea0
ifurther <55025025+ifurther@users.noreply.github.com>
2021-06-09 01:33:18 +0800

    change path

--- Challenge/addserviceaccount.sh
+++ Challenge/addserviceaccount.sh
@@ -2,6 +2,6 @@ gcloud iam service-accounts create my-account --display-name my-account
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:my-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role=roles/bigquery.admin
 gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID --member=serviceAccount:my-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role=roles/storage.objectAdmin
 gcloud iam service-accounts keys create key.json --iam-account=my-account@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com
-export GOOGLE_APPLICATION_CREDENTIALS=key.json
+export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/key.json
 
 #gsutil cp gs://$DEVSHELL_PROJECT_ID/analyze-images.py .
